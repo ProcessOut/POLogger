7c775ebce82b49f92490ece55f8251173f7fc302
Jeremy Lejoux <jeremy.lejoux@epita.fr>
2016-07-10 08:39:52 -0400

    Fix mail  LISENCE

---
+++ Example/Pods/Target Support Files/POSLogger/POSLogger-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_POSLogger : NSObject
+@end
+@implementation PodsDummy_POSLogger
+@end
---
+++ Example/Pods/Target Support Files/POSLogger/POSLogger-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double POSLoggerVersionNumber;
+FOUNDATION_EXPORT const unsigned char POSLoggerVersionString[];
+
